cad0cc6831469d50e2304d932815d9cb815903ae
mashovozka <mashovozka@gmail.com>
2025-10-15 22:32:47 +0200

    added cart schema

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,16 +1,16 @@
-import ProductCard from "./product-card";
-import { Product } from "@/types";
+import ProductCard from './product-card'
+import { Product } from '@/types'
 
 const ProductList = ({
   data,
   title,
   limit,
 }: {
-  data: Product[];
-  title?: string;
-  limit?: number;
+  data: Product[]
+  title?: string
+  limit?: number
 }) => {
-  const limitedData = limit ? data.slice(0, limit) : data;
+  const limitedData = limit ? data.slice(0, limit) : data
 
   return (
     <div className="my-10">
@@ -27,7 +27,7 @@ const ProductList = ({
         </div>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default ProductList;
+export default ProductList
